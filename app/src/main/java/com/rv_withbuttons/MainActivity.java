225c1c015efc9d4d3898e539370074e79adc0b1a
jamesb6313 <38964563+jamesb6313@users.noreply.github.com>
2019-11-30 20:11:00 -0400

    Initial commit - Base of project taken from https://demonuts.com/recyclerview-button/

---
+++ app/src/main/java/com/rv_withbuttons/MainActivity.java
@@ -0,0 +1,62 @@
+package com.rv_withbuttons;
+
+//Base of shoot n scoot project taken from url below
+//See: https://demonuts.com/recyclerview-button/
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    public static ArrayList<Model> modelArrayList;
+    private CustomAdapter customAdapter;
+    private Button btnnext;
+    private String[] fruitlist = new String[]{"Apples", "Oranges", "Potatoes", "Tomatoes", "Grapes"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler);
+        btnnext = (Button) findViewById(R.id.next);
+
+        modelArrayList = getModel();
+        customAdapter = new CustomAdapter(this);
+        recyclerView.setAdapter(customAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
+
+        btnnext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NextActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private ArrayList<Model> getModel(){
+        ArrayList<Model> list = new ArrayList<>();
+
+        //int numHoles = R.integer.num_holes;
+        for(int i = 0; i < 5; i++){
+
+            Model model = new Model();
+            model.setNumber(1);
+            model.setFruit(fruitlist[i]);
+            list.add(model);
+        }
+        return list;
+    }
+}
